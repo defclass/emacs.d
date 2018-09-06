85ca2075bb4c078c92068f0d674dafaead926abb
defclass <defclass@outlook.com>
2018-09-06 18:56:05 +0800

    add daily-md function

--- custom.el
+++ custom.el
@@ -13,3 +13,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(defun daily-md ()
+  (interactive)
+  (let ((daily-name (format-time-string "%Y-%m-%d")))
+    (find-file (format "%s.md" daily-name))))
